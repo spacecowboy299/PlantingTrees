c851955e5a24806eee6a8c388fda75202a1fc0a1
Scott <sbowendev@gmail.com>
2024-08-23 16:56:20 -0500

    Initial commit to github
    First program. Wanted to test my knowledge of using version control and how to publish java code.

---
+++ src/plantingtrees/PlantingTrees.java
@@ -0,0 +1,68 @@
+package plantingtrees;
+
+public class PlantingTrees {
+
+	public static void main(String[] args) {
+		/* Hello my name is Scott and welcome to my first Java program. This is the starting point in my Java projects here on Github. Each project is maintained with Git and follows best practice standars for Java programming. Thank you for considering me or viewing! */
+	    System.out.println("Hello, I'm Scott and I'm learning to program using Java!");
+	    System.out.println("Today, I will be planting some trees to help conserve our beautiful planet.");
+	    System.out.print("       ");
+	    System.out.print("       ");
+	    System.out.print("       ");
+	    System.out.print("       ");
+	    System.out.println("       ");
+
+
+	    System.out.print("   *   ");
+	    System.out.print("   *   ");
+	    System.out.print("   *   ");
+	    System.out.print("   *   ");
+	    System.out.println("   *   ");
+
+
+	    System.out.print(" ***** ");
+	    System.out.print(" ***** ");
+	    System.out.print(" ***** ");
+	    System.out.print(" ***** ");
+	    System.out.println(" ***** ");
+
+
+	    System.out.print(" ***** ");
+	    System.out.print(" ***** ");
+	    System.out.print(" ***** ");
+	    System.out.print(" ***** ");
+	    System.out.println(" ***** ");
+
+
+	    System.out.print("  ***  ");
+	    System.out.print("  ***  ");
+	    System.out.print("  ***  ");
+	    System.out.print("  ***  ");
+	    System.out.println("  ***  ");
+	    
+
+
+	    System.out.print("   *   ");
+	    System.out.print("   *   ");
+	    System.out.print("   *   ");
+	    System.out.print("   *   ");
+	    System.out.println("   *   ");
+	   
+
+
+	    System.out.print("   *   ");
+	    System.out.print("   *   ");
+	    System.out.print("   *   ");
+	    System.out.print("   *   ");
+	    System.out.println("   *   ");
+
+	    
+	    System.out.print("www*www");
+	    System.out.print("www*www");
+	    System.out.print("www*www");
+	    System.out.print("www*www");
+	    System.out.println("www*www");
+
+	}
+
+}
